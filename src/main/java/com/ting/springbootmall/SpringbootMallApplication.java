ddd47110c1a2471afa5193311bfb2792c0269209
ting <fisher1251@gmail.com>
2024-06-22 21:45:50 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/ting/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.ting.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ting/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
